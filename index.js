988e7ede16ac6162a3ee74dd615f6d3efa0e854c
Mahesh Pawate <mahesh@circle.news>
2020-07-09 20:34:43 +0530

    edit and delete completed

--- index.js
+++ index.js
@@ -1,8 +1,65 @@
-getAddButton=()=> {
-     var 
+todo=()=> {
+    let getInput=document.getElementById('takeInput');
+
+    let division=document.createElement('div');
+    let checkBox=document.createElement('input');
+        checkBox.setAttribute('type', 'checkbox');
+    let labelName=document.createElement('label');
+    let editButton=document.createElement('button');
+        editButton.textContent="edit"
+        editButton.addEventListener('click', editElement)
+    let deleteButton=document.createElement('button');
+        deleteButton.textContent="delete";
+        deleteButton.addEventListener('click', deleteElement)
+
+        labelName.textContent=getInput.value
+    division.append(checkBox,labelName,editButton,deleteButton);
+    showCompleted(division)
+}
+
+editElement=()=>{
+    let target=event.target;
+    let textLabel=target.previousElementSibling
+
+    let createInput=document.createElement('input')
+        createInput.textContent=textLabel.textContent;
+        textLabel.remove();
+
+        target.textContent="update";
+    target.removeEventListener('click',editElement);
+    target.addEventListener('click',updateElement);
+
+    let parent=target.parentElement
+    parent.insertBefore(createInput,target);
+}
+
+updateElement=()=> {
+    let target=event.target;
+    let textLabel=target.previousElementSibling
+
+    let createLabel=document.createElement('label')
+        createLabel.textContent=textLabel.value;
+        textLabel.remove();
+
+        target.textContent="Edit";
+    target.removeEventListener('click',updateElement);
+    target.addEventListener('click',editElement);
+
+    let parent=target.parentElement
+    parent.insertBefore(createLabel,target);
+}
+
+deleteElement=()=> {
+    let target=event.target
+    target.parentElement.remove()
+}
+
+showCompleted=(division)=> {
+    let completedDiv=document.getElementById('addTodo')
+    completedDiv.appendChild(division)
 }
 
 window.addEventListener("load", ()=>{
-    getAddButton()
+    let addButton=document.getElementById("add")
+    addButton.addEventListener('click',todo)
 })
-
