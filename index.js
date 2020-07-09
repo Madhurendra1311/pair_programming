163a02f1a14446586ff5f9a689429b68dcd2a18e
madhurendra1311 <kmadhu1311@gmail.com>
2020-07-09 21:25:49 +0530

    pair_problem

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ todo=()=> {
     let division=document.createElement('div');
     let checkBox=document.createElement('input');
         checkBox.setAttribute('type', 'checkbox');
+        checkBox.addEventListener('change',onchengeElement)
     let labelName=document.createElement('label');
     let editButton=document.createElement('button');
         editButton.textContent="edit"
@@ -15,6 +16,29 @@ todo=()=> {
         labelName.textContent=getInput.value
     division.append(checkBox,labelName,editButton,deleteButton);
     showCompleted(division)
+}
+
+onchengeElement=()=>{
+    let target=event.target
+    let getLabelName=target.nextElementSibling
+    target.parentElement.remove()
+    let completed=document.getElementById("completedList")
+    let division=document.createElement('div');
+    let checkBox=document.createElement('input');
+        checkBox.setAttribute('type', 'checkbox');
+        checkBox.addEventListener('change',onchengeElement)
+    let labelName=document.createElement('label');
+    let editButton=document.createElement('button');
+        editButton.textContent="edit"
+        editButton.addEventListener('click', editElement)
+    let deleteButton=document.createElement('button');
+        deleteButton.textContent="delete";
+        deleteButton.addEventListener('click', deleteElement)
+
+        labelName.textContent=getLabelName.textContent
+    division.append(checkBox,labelName,editButton,deleteButton);
+    completed.appendChild(division)
+
 }
 
 editElement=()=>{
@@ -58,8 +82,26 @@ showCompleted=(division)=> {
     let completedDiv=document.getElementById('addTodo')
     completedDiv.appendChild(division)
 }
+let flag=true
+toggler=()=>{
+    // console.log("hide")
+    
+    let getCompleted= document.getElementById('completedList')
+
+    if(flag){
+        
+        getCompleted.style.display="block";
+        console.log(getCompleted)
+        flag=false;
+    }else{
+        getCompleted.style.display="none"
+        flag=true;
+    }
+}
 
 window.addEventListener("load", ()=>{
     let addButton=document.getElementById("add")
     addButton.addEventListener('click',todo)
+    let getToggle= document.getElementById('toggle')
+    getToggle.addEventListener('click',toggler)
 })
